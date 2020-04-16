1f18657f75773a16e067be65ace63b923b5448ae
Justin Snider <sniderjustin@gmail.com>
2020-04-16 12:55:15 -0400

    delete project files

--- CodeSearchNet/src/encoders/__init__.py
+++
@@ -1,6 +0,0 @@
-from .encoder import Encoder, QueryType
-from .nbow_seq_encoder import NBoWEncoder
-from .rnn_seq_encoder import RNNEncoder
-from .self_att_encoder import SelfAttentionEncoder
-from .conv_seq_encoder import ConvolutionSeqEncoder
-from .conv_self_att_encoder import ConvSelfAttentionEncoder
